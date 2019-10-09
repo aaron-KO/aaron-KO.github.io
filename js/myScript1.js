0274275a1b0262778ea2107b438ee221aabfcb75
aaron-KO <56333396+aaron-KO@users.noreply.github.com>
2019-10-09 01:17:12 -0400

    css and json

---
+++ js/myScript1.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    var b = document.getElementById("bar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        b.className = "fas fa-times";
+    } else {
+        x.className = "topnav";
+        b.className = "fa fa-plus";
+    }
+}
